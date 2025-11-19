575b9e7692a5f643de88e8f4180bfa850d8af014
Ivan <ivan-kostyulin@yandex.ru>
2025-11-19 11:58:59 +0300

    fix: api url

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -3,13 +3,9 @@ export const getApiUrl = (): string => {
 
 	if (hostname === 'localhost') {
 		return 'http://10.242.221.0:8000/api';
-	} else if (hostname === 'cvo-test.emiit.ru') {
-		return 'https://cvo-test-api.emiit.ru/api/v1';
-	} else if (hostname === 'cvo.emiit.ru') {
-		return 'https://cvo-api.emiit.ru/api/v1';
+	} else {
+		return 'https://pd.emiit.ru/api';
 	}
-
-	return 'http://10.242.224.105:8000/api/v1';
 };
 
 export const API_URL = getApiUrl();
